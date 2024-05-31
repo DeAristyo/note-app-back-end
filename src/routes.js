1ac75b65a721664a7d9e62210c9f5bf440fe0a89
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2024-05-31 19:35:05 +0700

    Task: Finished test

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,46 @@
-const { addNoteHandler } = require('./handler');
+import {
+    addNoteHandler,
+    getNoteWithIdHandler,
+    getAllNotesHandler,
+    deleteNoteHandler,
+    updateNoteHandler
+} from './handler.js';
+// import notes from './notes.js';
+
 const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello NAYA INI BISA!';
+        }
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteWithIdHandler,
+    },
     {
         method: 'POST',
         path: '/notes',
         handler: addNoteHandler,
     },
-   ];
-    
-   module.exports = routes;
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: updateNoteHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteHandler,
+    },
+];
+
+export default routes;
